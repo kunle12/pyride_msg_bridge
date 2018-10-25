7b867d04911ccf97cc76d3e4f1542bcf6907fcce
Xun Wang <wang.xun@gmail.com>
2018-10-25 15:21:38 +1100

    ok. make a silly check whether the status_text is a plain string or a json object string for example.

--- src/pyride_msg_bridge.cpp
+++ src/pyride_msg_bridge.cpp
@@ -122,7 +122,12 @@ void PyRIDEMsgBridge::nodeStatusCB( const pyride_common_msgs::NodeStatusConstPtr
 
   // put in a json string format
   ss << "{\"node\": \"" << msg->node_id << "\", \"timestamp\": " << ((double)msg->header.stamp.sec + (double)msg->header.stamp.nsec / 1E9);
-  ss << ", \"message\": " << msg->status_text << ", \"priority\": " << (int)msg->priority << "}";
+  if (msg->status_text.length() > 2 && (msg->status_text[0] =='{' || msg->status_text[0] =='[') ) {
+    ss << ", \"message\": " << msg->status_text << ", \"priority\": " << (int)msg->priority << "}";
+  }
+  else {
+    ss << ", \"message\": \"" << msg->status_text << "\", \"priority\": " << (int)msg->priority << "}";
+  }
 
   NodeStatus = ss.str();
   PYCONNECT_ATTRIBUTE_UPDATE( NodeStatus );
